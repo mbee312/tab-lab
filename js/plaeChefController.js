3c36034fa114d34917233f941f22391e0a730a5d
albert llavore <albert@goplae.com>
2014-11-17 14:11:04 -0800

    add 'ngDragDrop'
    add 'ngDragDrop'

--- js/plaeChefController.js
+++ js/plaeChefController.js
@@ -2,12 +2,15 @@
   'use strict';
 
   // Declare app level module which depends on views, and components
-  var plaeChefApp = angular.module('plaeChefApp', ["ngAnimate"]);
+  var plaeChefApp = angular.module('plaeChefApp', ['ngDragDrop']);
 
 
   plaeChefApp.controller('PlaeChefController', ['$scope', '$http', function($scope, $http){
     var canvas = document.getElementById('plae-chef-canvas');
     var context = canvas.getContext('2d');
+    // Tabs on canvas List Arrays
+    $scope.tabs = [];
+    $scope.basket =[];
 
     $http.get('product/shoeStyles.json').success(function(data) {
       $scope.shoesStyles = data;
@@ -39,7 +42,6 @@
       // Restore the transform
       context.restore();
 
-      console.log("im in the makeBaseImage function");
       var base_image = new Image();
       base_image.src = imageUrl;
       if(specialSize === 0){
@@ -49,22 +51,137 @@
       }
     }; //end makeBaseImage
 
-    console.log("testing!");
+
+        // Add a Item to the list
+    this.addTab = function (tab, event) {
+
+        $scope.tabs.push(tab);
+
+
+    };
+
+
+
 
   }]);
 
-  plaeChefApp.controller('StylesTabController', function(){
+
+  plaeChefApp.controller('StylesTabController', function($scope){
     this.stylestab = '';
     this.setTab = function(newValue){
       this.stylestab = newValue;
     };
 
     this.isSet = function(tabName){
-      return this.stylestab === tabName;
+        return this.stylestab === tabName;
+
     };
 
+      /*
+    $scope.handleDrop = function() {
+        alert('Item has been dropped');
+    } */
+
+
   });
-  
+
+    /*
+  plaeChefApp.directive('draggable', function() {
+    return function(scope, element) {
+        // this gives us the native JS object
+        var el = element[0];
+
+        el.draggable = true;
+
+        el.addEventListener(
+            'dragstart',
+            function(e) {
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData('Text', this.id);
+                this.classList.add('drag');
+                return false;
+            },
+            false
+        );
+
+        el.addEventListener(
+            'dragend',
+            function(e) {
+                this.classList.remove('drag');
+                return false;
+            },
+            false
+        );
+    }
+  });
+
+  plaeChefApp.directive('droppable', function() {
+    return {
+        scope: {
+          drop: '&', //parent
+          bin: '=' //bi-directional scope
+        },
+        link: function(scope, element) {
+          // again we need the native object
+          var el = element[0];
+
+          el.addEventListener(
+            'dragover',
+            function(e) {
+              e.dataTransfer.dropEffect = 'move';
+              // allows us to drop
+              if (e.preventDefault) e.preventDefault();
+              this.classList.add('over');
+              return false;
+            },
+            false
+          );
+
+          el.addEventListener(
+            'dragenter',
+            function(e) {
+              this.classList.add('over');
+              return false;
+            },
+          false
+          );
+
+          el.addEventListener(
+            'dragleave',
+            function(e) {
+              this.classList.remove('over');
+              return false;
+            },
+            false
+          );
+          el.addEventListener(
+            'drop',
+            function(e) {
+              // Stops some browsers from redirecting.
+              if (e.stopPropagation) e.stopPropagation();
+
+              this.classList.remove('over');
+
+              var binId = this.id;
+              var item = document.getElementById(e.dataTransfer.getData('Text'));
+              this.appendChild(item);
+
+              // call the drop passed drop function
+              scope.$apply(function(scope) {
+                var fn = scope.drop();
+                if ('undefined' !== typeof fn) {
+                  fn(item.id, binId);
+                  }
+                });
+              return false;
+          },
+          false
+          );
+        }//end link
+    }
+  });
+
+  */
 
 })();
 
