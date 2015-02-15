a8b3fbe0249781b4fd7a9e635f49f9ae222b7533
albert llavore <albert@goplae.com>
2015-02-15 01:28:02 -0800

    add radio buttons to survey

--- js/plaeChefController.js
+++ js/plaeChefController.js
@@ -895,7 +895,8 @@
             email: '',
             question_1: '' ,
             question_2: '' ,
-            question_3: ''
+            question_3: '',
+            question_4: ''
         };
 
     }])
