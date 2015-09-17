e2616d65bf29323bd8f2a8e4e6fc162d14994ca1
albert llavore <albert@goplae.com>
2015-09-17 15:56:27 -0700

    fix lighting, camera, dimensions

--- app/canvas/sceneController.js
+++ app/canvas/sceneController.js
@@ -5,13 +5,19 @@
     'use strict';
 
     var app = angular.module('tabLabApp');
-    app.controller('SceneCtrl',
-        ['$scope',
-            'tabLabProperties',
-            'sizeProperties',
-            function($scope, tabLabProperties, sizeProperties) {
-
-
+    app.controller('SceneCtrl', ['$scope', 'tabLabProperties', 'sizeProperties', function($scope, tabLabProperties, sizeProperties) {
+        var group, text, plane;
+        var targetRotationX = 0;
+        var targetRotationOnMouseDownX = 0;
+        var targetRotationY = 0;
+        var targetRotationOnMouseDownY = 0;
+        var mouseX = 0;
+        var mouseXOnMouseDown = 0;
+        var mouseY = 0;
+        var mouseYOnMouseDown = 0;
+        var windowHalfX = window.innerWidth / 2;
+        var windowHalfY = window.innerHeight / 2;
+        var finalRotationY;
 }]);
 
 }());
