c59c6898b9faa4c0e7ff856fc2d999871cab0c60
Rashad Itani <rashad@goplae.com>
2015-09-25 23:07:37 -0700

    Add Refresh cart menu and top link, and notifications when items added

--- app/cart/cartController.js
+++ app/cart/cartController.js
@@ -29,7 +29,7 @@
             }
         };
     });
-    app.controller('CartCtrl', ['$scope', '$http', 'tabLabProperties', 'sizeProperties', 'cartProperties', function ($scope, $http, tabLabProperties, sizeProperties, cartProperties) {
+    app.controller('CartCtrl', ['$scope', '$http', '$q', 'tabLabProperties', 'sizeProperties', 'cartProperties', function ($scope, $http, $q, tabLabProperties, sizeProperties, cartProperties) {
 
         $scope.$on('shoe-set', function(event, args) {
             cartProperties.updateCart(tabLabProperties.getShoe(), "shoe");
@@ -79,60 +79,97 @@
             var url = '/ajax/index/add/product/';
             var shoe = tabLabProperties.getShoe();
             var tabs = {};
+            var calls = [];
+            var callsData = [];
             for (var i = 0; i < 4; i++) {
                 var tab = tabLabProperties.getTab(i);
                 tabs[tab.product_id] = (tabs[tab.product_id] || 0) + 1;
             }
-            if (shoe) {
-                var shoeSize = sizeProperties.getShoeSize();
-                if (!shoeSize) {
-                    alert("Please select a shoe size.");
+            var shoeSize = sizeProperties.getShoeSize();
+            if (shoe && !shoeSize) {
+                toastr.error("Please select a shoe size.");
+                return;
+            }
+            if (!_.isEmpty(tabs)) {
+                var tabSize = sizeProperties.getTabSize();
+                if (!tabSize) {
+                    toastr.error("Please select a tab size.");
                     return;
                 }
+            }
+            if (shoe) {
                 var shoeParams = {'isAjax':1};
                 shoeParams["super_attribute[" + $scope.dataOptions["code"]["color"] + "]"] = shoe.color;
                 shoeParams["super_attribute[" + $scope.dataOptions["code"]["size"] + "]"] = shoeSize.id;
                 var shoeurl = url + shoe.product_id;
 
-                var responsePromise = $http.get(shoeurl, {'params': shoeParams});
-
-                responsePromise.success(function(data, status, headers, config) {
-                    if (status == 200) {
-                        console.log(data);
-                    } else {
-                        console.log("Adding " + shoe.id + " failed!");
-                    }
-                });
-                responsePromise.error(function(data, status, headers, config) {
-                    console.log("Adding " + shoe.id + " failed!");
-                });
+                callsData.push(shoe);
+                calls.push(function() {return $http.get(shoeurl, {'params': shoeParams})});
             }
-            if (!_.isEmpty(tabs)) {
-                var tabSize = sizeProperties.getTabSize();
-                if (!tabSize) {
-                    alert("Please select a tab size.");
-                    return;
-                }
-            }
-            _.each(tabs, function(tabCount, tabId){
+            for (var tabId in tabs) {
+                var tabCount = tabs[tabId];
                 var tabUrl = url + tabId;
                 var tabParams = {'isAjax':1};
                 tabParams["super_attribute[" + $scope.dataOptions["code"]["tabsize"] + "]"] = tabSize.id;
                 tabParams["qty"] = tabCount / 2;
 
-                var tabResponsePromise = $http.get(tabUrl, {'params': tabParams});
+                (function(tabUrl, tabParams){
+                    callsData.push({"id": tabId});
+                    calls.push(function() {
+                        return $http.get(tabUrl, {'params': tabParams})
+                    });
+                })(tabUrl, tabParams);
+            }
 
-                tabResponsePromise.success(function(data, status, headers, config) {
-                    if (status == 200) {
-                        console.log(data);
-                    } else {
-                        console.log("Adding " + tabSelected[0] + " failed!");
+// serially add the products
+            var data = [];
+            if (calls[0]) {
+                calls[0]().then(function(result){
+                    if (result) data.push(result.data);
+                    return calls[1] ? calls[1]() : null;
+                }).then(function(result){
+                    if (result) data.push(result.data);
+                    return calls[2] ? calls[2]() : null;
+                }).then(function(result){
+                    if (result) data.push(result.data);
+                }).finally(function(error){
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        if(data.length > 0) {
+                            var lastDataIndex = data.length - 1;
+                            var header = data[lastDataIndex].header;
+                            while (!header) {
+                                lastDataIndex = lastDataIndex - 1;
+                                header = data[lastDataIndex].header;
+                            }
+                            if (header) {
+                                var $header = $($.parseHTML(header));
+                                $("#cart").html($header.filter("#cart").html());
+                                $(".cart-link").html($header.find(".cart-link").html());
+                            }
+                            for (var l in data) {
+                                toastr[data[l].status.toLowerCase()](data[l].message);
+                            }
+
+                            console.log(data);
+                        }
                     }
                 });
-                tabResponsePromise.error(function(data, status, headers, config) {
-                    console.log("Adding " + tabSelected[0] + " failed!");
-                });
-            });
+            }
+
+
+// Add all products at once
+//            var promises = [];
+//            for(var a in calls) {
+//                promises.push(calls[a]());
+//            }
+//            $q.all(promises).then(function(results) {
+//                /* your logic here */
+//                console.log(results);
+//            });
+
         }
 
     }]);
